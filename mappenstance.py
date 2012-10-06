e24a92f263a524141316bdb113ac3c7719507b4f
Steve Kendall <kendall@wereware.com>
2012-10-06 14:11:04 -0400

    i still think this looks best, and it is easiest to make such tile assignments now than try to adjust them later

--- mappenstance.py
+++ mappenstance.py
@@ -28,7 +28,10 @@ class Mapp:
 	DEFAULT_MAX_ROOM_WIDTH = 40 
 	DEFAULT_MAX_ROOM_HEIGHT = 40 
 	DEFAULT_MAX_TRIES = 30
-	WALL_SYMBOL = 'O' 
+	HORIZONTAL_WALL_SYMBOL = '_' 
+	VERTICAL_WALL_SYMBOL = '|' 
+	TOP_CORNER_SYMBOL = ' ' 
+	BOTTOM_CORNER_SYMBOL = '|' 
 	EMPTY_SYMBOL = '~' 
 	FLOOR_SYMBOL = '.'
 
@@ -90,6 +93,7 @@ class Mapp:
 
 	def addRooms(self):
 		ctr = 0
+		#TODO: appraise this
 		while True:
 			for i in range(0, self.maxRooms):
 				ctr = i
@@ -151,19 +155,17 @@ class Mapp:
 				startX, startY, endX, endY)
 		# first we draw the walls horizontally
 		for j in range(startX, endX + 1):
-			map[startY][j].ascii = Mapp.WALL_SYMBOL
-			map[endY][j].ascii = Mapp.WALL_SYMBOL
+			map[startY][j].ascii = Mapp.HORIZONTAL_WALL_SYMBOL
+			map[endY][j].ascii = Mapp.HORIZONTAL_WALL_SYMBOL
 		# then we draw the walls vertically
 		for i in range(startY, endY + 1):
-			map[i][startX].ascii = Mapp.WALL_SYMBOL
-			map[i][endX].ascii = Mapp.WALL_SYMBOL 
+			map[i][startX].ascii = Mapp.VERTICAL_WALL_SYMBOL
+			map[i][endX].ascii = Mapp.VERTICAL_WALL_SYMBOL 
 		# then we draw the corners
-		"""
 		map[startY][startX].ascii = Mapp.TOP_CORNER_SYMBOL
 		map[startY][endX].ascii = Mapp.TOP_CORNER_SYMBOL
 		map[endY][startX].ascii = Mapp.BOTTOM_CORNER_SYMBOL
 		map[endY][endX].ascii = Mapp.BOTTOM_CORNER_SYMBOL
-		"""
 		# then we draw the innards
 		for i in range(startY + 1, endY):
 			for j in range(startX + 1, endX):
