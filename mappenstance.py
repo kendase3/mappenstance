763042533916659e1fc13ab7d93e532b7c0884b3
Steve Kendall <kendall@wereware.com>
2012-10-06 00:22:29 -0400

    ok now fully inside this map object

--- mappenstance.py
+++ mappenstance.py
@@ -53,7 +53,7 @@ class CellMap:
 		while True:
 			for i in range(0, MAX_ROOMS):
 				ctr = i
-				if not addRoom(self.cells):
+				if not self.addRoom():
 					break 
 			if ctr >= MIN_ROOMS:
 				self.roomCount = ctr
@@ -61,54 +61,55 @@ class CellMap:
 			else:
 				self.reset()
 
-def addRoom(map):
-	"""
-		returns True if it could add a room with ease,
-				False if it got all tuckered out
-	"""
-	#TODO: could add wrapper function to ensure min/max # of rooms and just restart
-	#		level creation if it's a wash 
+	def addRoom(self):
+		"""
+			returns True if it could add a room with ease,
+					False if it got all tuckered out
+		"""
+		#TODO: could add wrapper function to ensure min/max # of rooms and just restart
+		#		level creation if it's a wash 
 
-	# 2 for walls
-	maxStartX = MAP_WIDTH - 2 - MIN_ROOM_WIDTH
-	maxStartY = MAP_HEIGHT - 2 - MIN_ROOM_HEIGHT
-	numTries = 0
-	while True:
-		startX = random.randint(0, maxStartX)  
-		startY = random.randint(0, maxStartY)  
-		# 3 because 2 for walls + at least 1 for inside
-		endX = random.randint(startX + 3, min(MAP_WIDTH - 1, startX + MAX_ROOM_WIDTH + 1)) 
-		endY = random.randint(startY + 3, min(MAP_HEIGHT - 1, startY + MAX_ROOM_HEIGHT + 1)) 
-		# we ensure it does not collide with existing rooms
-		# by checking entire proposed innards
-		passes = True
+		# 2 for walls
+		map = self.cells
+		maxStartX = MAP_WIDTH - 2 - MIN_ROOM_WIDTH
+		maxStartY = MAP_HEIGHT - 2 - MIN_ROOM_HEIGHT
+		numTries = 0
+		while True:
+			startX = random.randint(0, maxStartX)  
+			startY = random.randint(0, maxStartY)  
+			# 3 because 2 for walls + at least 1 for inside
+			endX = random.randint(startX + 3, min(MAP_WIDTH - 1, startX + MAX_ROOM_WIDTH + 1)) 
+			endY = random.randint(startY + 3, min(MAP_HEIGHT - 1, startY + MAX_ROOM_HEIGHT + 1)) 
+			# we ensure it does not collide with existing rooms
+			# by checking entire proposed innards
+			passes = True
+			for i in range(startY, endY + 1):
+				for j in range(startX, endX + 1):
+					if map[i][j].ascii != EMPTY_SYMBOL:
+						passes = False
+			# if the perimeter is all wall, then we can dig out a room
+			if passes:
+				break
+			else:
+				numTries += 1
+				#print "numTries = %d" % numTries
+			if numTries == MAX_TRIES:
+				return False
+		print "decided to make a room between x=%d,y=%d and x=%d,y=%d" % (
+				startX, startY, endX, endY)
+		# first we draw the walls horizontally
+		for j in range(startX, endX + 1):
+			map[startY][j].ascii = WALL_SYMBOL
+			map[endY][j].ascii = WALL_SYMBOL
+		# then we draw the walls vertically
 		for i in range(startY, endY + 1):
-			for j in range(startX, endX + 1):
-				if map[i][j].ascii != EMPTY_SYMBOL:
-					passes = False
-		# if the perimeter is all wall, then we can dig out a room
-		if passes:
-			break
-		else:
-			numTries += 1
-			#print "numTries = %d" % numTries
-		if numTries == MAX_TRIES:
-			return False
-	print "decided to make a room between x=%d,y=%d and x=%d,y=%d" % (
-			startX, startY, endX, endY)
-	# first we draw the walls horizontally
-	for j in range(startX, endX + 1):
-		map[startY][j].ascii = WALL_SYMBOL
-		map[endY][j].ascii = WALL_SYMBOL
-	# then we draw the walls vertically
-	for i in range(startY, endY + 1):
-		map[i][startX].ascii = WALL_SYMBOL
-		map[i][endX].ascii = WALL_SYMBOL 
-	# then we draw the innards
-	for i in range(startY + 1, endY):
-		for j in range(startX + 1, endX):
-			map[i][j].ascii = FLOOR_SYMBOL 
-	return True
+			map[i][startX].ascii = WALL_SYMBOL
+			map[i][endX].ascii = WALL_SYMBOL 
+		# then we draw the innards
+		for i in range(startY + 1, endY):
+			for j in range(startX + 1, endX):
+				map[i][j].ascii = FLOOR_SYMBOL 
+		return True
 
 map = CellMap(MAP_WIDTH, MAP_HEIGHT) 
 map.prnt() 
