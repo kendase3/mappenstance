87257b06e19a8c352f0b87da8c1da94298a0aec4
Steve Kendall <kendall@wereware.com>
2012-10-30 05:20:07 -0400

    this whole business is really coming along rather nicely would not you say?

--- populator.py
+++ populator.py
@@ -1,8 +1,63 @@
 
 import random
 
+from cell import Cell
+from room import Room
+
+
 class Populator:
+	DEFAULT_STAIR_NUM = 1  # we default to one up, one down 
 	def __init__(self):
 		pass
-	def addStairs(self, mapp):
-		print repr(mapp.roomList)
+
+	def hasUnassignedRoom(self, roomList):
+		for room in roomList:
+			if room.type == None:
+				return True
+		return False
+
+	def hasUnassignedCell(self, room, mapCells):
+		for i, row in enumerate(mapCells[room.y:room.y+room.height]):
+			for j, cell in enumerate(row[room.x:room.x+room.width]):
+					# then it's in the room	
+					if cell.ascii == Cell.FLOOR_SYMBOL:
+						return True 
+		return False
+
+	def getUnassignedRoom(self, roomList):
+		if not self.hasUnassignedRoom(roomList):
+			print 'ERROR: insufficient rooms'
+			return
+		index = None 
+		while index == None or roomList[index].type != None:
+			index = random.randint(0, len(roomList) - 1)
+		return roomList[index]
+
+	def getUnassignedCell(self, room, mapCells):
+		if not self.hasUnassignedCell(room, mapCells):
+			print 'ERROR: insufficient cells in selected room'
+			return
+		xOffset = None
+		# while there's something in the cell we've selected, get a new one
+		while xOffset == None or ( mapCells[room.y + yOffset][room.x + xOffset].ascii != Cell.FLOOR_SYMBOL): 
+			xOffset = random.randint(0, room.width - 1)
+			yOffset = random.randint(0, room.height - 1)
+		return mapCells[room.y + yOffset][room.x + xOffset] 
+		
+	def addStairs(self, mapp, numStairsUp=None, 
+				numStairsDown=None):
+		if numStairsUp == None:
+			numStairsUp = Populator.DEFAULT_STAIR_NUM
+		if numStairsDown == None:
+			numStairsDown = Populator.DEFAULT_STAIR_NUM
+		for i in range(0, numStairsUp):
+			targetRoom = self.getUnassignedRoom(mapp.roomList)	
+			targetCell = self.getUnassignedCell(targetRoom, mapp.cells)
+			targetCell.ascii = Cell.STAIRS_UP_SYMBOL
+			targetRoom.type = 'stairsup' 
+		for i in range(0, numStairsDown):
+			targetRoom = self.getUnassignedRoom(mapp.roomList)	
+			targetCell = self.getUnassignedCell(targetRoom, mapp.cells)
+			targetCell.ascii = Cell.STAIRS_DOWN_SYMBOL
+			targetRoom.type = 'stairsdown' 
+		
--- room.py
+++ room.py
@@ -1,11 +1,14 @@
 
 class Room:
+	STAIRSUP_TYPE = 'stairsup'
+	STAIRSDOWN_TYPE = 'stairsdown'
 	def __init__(self, id, x, y, width, height):
 		self.id = id
 		self.x = x 
 		self.y = y 
 		self.width = width 
 		self.height = height 
+		self.type = None # what category of room is it? 
 	
 	def __repr__(self):
 		return "Room: id=%d, x=%d, y=%d, width=%d, height=%d" % (
